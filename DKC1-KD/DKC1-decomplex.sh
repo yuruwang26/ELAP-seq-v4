a88d373f9fb012cda60e00358438d2124cb3feb9
yuruwang26 <43427465+yuruwang26@users.noreply.github.com>
2026-01-19 15:40:39 -0700

    Update DKC1-decomplex.sh

--- DKC1-KD/DKC1-decomplex.sh
+++ DKC1-KD/DKC1-decomplex.sh
@@ -1,8 +1,8 @@
 
 
-/home/yuruwang/Tools/anaconda3/bin/bbmerge.sh in1=YW-6s-1120-S1_R1.fastq.gz in2=YW-6s-1120-S1_R2.fastq.gz out=YW_S1_merge.fastq.gz
+/home/yuruwang/Tools/anaconda3/bin/bbmerge.sh in1=YW-S1_R1.fastq.gz in2=YW-S1_R2.fastq.gz out=YW_S1_merge.fastq.gz
 
-/home/yuruwang/Tools/anaconda3/bin/bbmerge.sh in1=YW-6s-1120-S2_R1.fastq.gz in2=YW-6s-1120-S2_R2.fastq.gz out=YW_S2_merge.fastq.gz
+/home/yuruwang/Tools/anaconda3/bin/bbmerge.sh in1=YW-S2_R1.fastq.gz in2=YW-S2_R2.fastq.gz out=YW_S2_merge.fastq.gz
 
 gunzip YW_S1_merge.fastq.gz
 gunzip YW_S2_merge.fastq.gz
@@ -17,5 +17,6 @@ seqkit grep -s -r -p "^TCT" YW_S5_merge.fastq -o ./sorted/HEK-sictrl-IP-IV-rep1.
 seqkit grep -s -r -p "^GAC" YW_S5_merge.fastq -o ./sorted/HEK-sictrl-IP-IV-rep2.fastq
 seqkit grep -s -r -p "^CTG" YW_S5_merge.fastq -o ./sorted/HEK-siDKC1-IP-IV-rep1.fastq
 seqkit grep -s -r -p "^ACA" YW_S5_merge.fastq -o ./sorted/HEK-siDKC1-IP-IV-rep2.fastq
+
 
 
