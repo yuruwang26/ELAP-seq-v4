44d4cc99308913922dc9078806f4a71c3318a43a
yuruwang26 <43427465+yuruwang26@users.noreply.github.com>
2026-02-12 23:05:48 -0700

    Refactor DKC1-decomplex.sh for clarity and consistency

--- DKC1-KD/DKC1-decomplex.sh
+++ DKC1-KD/DKC1-decomplex.sh
@@ -9,14 +9,15 @@ gunzip YW_S2_merge.fastq.gz
 
 ###need to convert .gz to .fastq
 seqkit grep -s -r -p "^TCT" YW_S1_merge.fastq -o ./sorted/HEK-sictrl-input-IV-rep1.fastq
-seqkit grep -s -r -p "^GAC" YW_S1_merge.fastq -o ./sorted/HEK-sictrl-input-IV-rep2.fastq
+seqkit grep -s -r -p "^ACA" YW_S1_merge.fastq -o ./sorted/HEK-sictrl-input-IV-rep2.fastq
 seqkit grep -s -r -p "^CTG" YW_S1_merge.fastq -o ./sorted/HEK-siDKC1-input-IV-rep1.fastq
-seqkit grep -s -r -p "^ACA" YW_S1_merge.fastq -o ./sorted/HEK-siDKC1-input-IV-rep2.fastq
+seqkit grep -s -r -p "^GAC" YW_S1_merge.fastq -o ./sorted/HEK-siDKC1-input-IV-rep2.fastq
 
 seqkit grep -s -r -p "^TCT" YW_S5_merge.fastq -o ./sorted/HEK-sictrl-IP-IV-rep1.fastq
-seqkit grep -s -r -p "^GAC" YW_S5_merge.fastq -o ./sorted/HEK-sictrl-IP-IV-rep2.fastq
+seqkit grep -s -r -p "^ACA" YW_S5_merge.fastq -o ./sorted/HEK-sictrl-IP-IV-rep2.fastq
 seqkit grep -s -r -p "^CTG" YW_S5_merge.fastq -o ./sorted/HEK-siDKC1-IP-IV-rep1.fastq
-seqkit grep -s -r -p "^ACA" YW_S5_merge.fastq -o ./sorted/HEK-siDKC1-IP-IV-rep2.fastq
+seqkit grep -s -r -p "^GAC" YW_S5_merge.fastq -o ./sorted/HEK-siDKC1-IP-IV-rep2.fastq
+
 
 
 
